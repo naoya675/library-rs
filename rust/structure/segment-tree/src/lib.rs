5784572261107d64bde24769cdec41ee4b7c7460
naoya675 <64210874+naoya675@users.noreply.github.com>
2024-03-11 14:31:54 +0900

    add rust/structure

---
+++ rust/structure/segment-tree/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod segment_tree;
+pub use segment_tree::SegmentTree;
---
+++ rust/verification/library-checker/src/bin/unionfind.rs
@@ -0,0 +1,25 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+use proconio::input;
+
+use union_find::UnionFind;
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        tuv: [(usize, usize, usize); q],
+    }
+    let mut uf = UnionFind::new(n);
+    for (t, u, v) in tuv {
+        match t {
+            0 => {
+                uf.merge(u, v);
+            }
+            1 => {
+                println!("{}", if uf.same(u, v) { 1 } else { 0 });
+            }
+            _ => unreachable!(),
+        }
+    }
+}
