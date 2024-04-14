a0545a41d61bcd46dbb7c2960a8ca9e4a89654d6
naoya675 <64210874+naoya675@users.noreply.github.com>
2024-04-15 00:28:32 +0900

    add graph/topological-sort

---
+++ graph/topological-sort/src/lib.rs
@@ -0,0 +1,84 @@
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, VecDeque};
+
+#[derive(Debug, Clone)]
+pub struct TopologicalSort {
+    size: usize,
+    graph: Vec<Vec<usize>>,
+}
+
+impl TopologicalSort {
+    pub fn new(size: usize) -> Self {
+        Self {
+            size,
+            graph: vec![vec![]; size],
+        }
+    }
+
+    pub fn add_edge(&mut self, from: usize, to: usize) {
+        self.graph[from].push(to);
+    }
+
+    pub fn topological_sort(&mut self) -> Vec<usize> {
+        let mut indegree = vec![0; self.size];
+        for from in 0..self.size {
+            for i in 0..self.graph[from].len() {
+                let to = self.graph[from][i];
+                indegree[to] += 1;
+            }
+        }
+        let mut heap = BinaryHeap::new();
+        for from in 0..self.size {
+            if indegree[from] == 0 {
+                heap.push(Reverse(from));
+            }
+        }
+        let mut res = vec![];
+        while let Some(Reverse(from)) = heap.pop() {
+            res.push(from);
+            for i in 0..self.graph[from].len() {
+                let to = self.graph[from][i];
+                indegree[to] -= 1;
+                if indegree[to] == 0 {
+                    heap.push(Reverse(to));
+                }
+            }
+        }
+        if res.len() != self.size {
+            return vec![];
+        }
+        res
+    }
+}
+
+// lexicographic order
+pub fn topological_sort(size: usize, graph: &Vec<Vec<usize>>) -> Vec<usize> {
+    let mut indegree = vec![0; size];
+    for from in 0..size {
+        for i in 0..graph[from].len() {
+            let to = graph[from][i];
+            indegree[to] += 1;
+        }
+    }
+    let mut heap = BinaryHeap::new();
+    for from in 0..size {
+        if indegree[from] == 0 {
+            heap.push(Reverse(from));
+        }
+    }
+    let mut res = vec![];
+    while let Some(Reverse(from)) = heap.pop() {
+        res.push(from);
+        for i in 0..graph[from].len() {
+            let to = graph[from][i];
+            indegree[to] -= 1;
+            if indegree[to] == 0 {
+                heap.push(Reverse(to));
+            }
+        }
+    }
+    if res.len() != size {
+        return vec![];
+    }
+    res
+}
