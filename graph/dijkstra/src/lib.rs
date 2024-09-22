6825e582a10adbdca34f68acd8c32c91b2442350
naoya675 <64210874+naoya675@users.noreply.github.com>
2024-09-23 04:04:04 +0900

    fix graph/dijkstra

--- graph/dijkstra/src/lib.rs
+++ graph/dijkstra/src/lib.rs
@@ -37,7 +37,7 @@ impl Dijkstra {
     pub fn dijkstra(&mut self, s: usize) -> Vec<Cost> {
         let mut dist = vec![Self::INF; self.size];
         dist[s] = 0;
-        let mut heap = BinaryHeap::new();
+        let mut heap: BinaryHeap<(i64, usize)> = BinaryHeap::new();
         heap.push((-dist[s], s));
         while let Some((d, from)) = heap.pop() {
             if dist[from] < -d {
@@ -52,12 +52,33 @@ impl Dijkstra {
         }
         dist
     }
+
+    pub fn dijkstra_prev(&mut self, s: usize) -> (Vec<Cost>, Vec<usize>) {
+        let mut dist = vec![Self::INF; self.size];
+        let mut prev = vec![self.size; self.size];
+        dist[s] = 0;
+        let mut heap: BinaryHeap<(i64, usize)> = BinaryHeap::new();
+        heap.push((-dist[s], s));
+        while let Some((d, from)) = heap.pop() {
+            if dist[from] < -d {
+                continue;
+            }
+            for edge in &self.graph[from] {
+                if dist[edge.to] > dist[from] + edge.cost {
+                    dist[edge.to] = dist[from] + edge.cost;
+                    prev[edge.to] = from;
+                    heap.push((-dist[edge.to], edge.to));
+                }
+            }
+        }
+        (dist, prev)
+    }
 }
 
 pub fn dijkstra(size: usize, graph: &Vec<Vec<Edge>>, s: usize) -> Vec<Cost> {
     let mut dist = vec![Cost::MAX / 2; size];
     dist[s] = 0;
-    let mut heap = BinaryHeap::new();
+    let mut heap: BinaryHeap<(i64, usize)> = BinaryHeap::new();
     heap.push((-dist[s], s));
     while let Some((d, from)) = heap.pop() {
         if dist[from] < -d {
