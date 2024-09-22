86661ec0d9b4c2a8e23f268bc08fe9e4a0456eee
naoya675 <64210874+naoya675@users.noreply.github.com>
2024-09-23 04:03:48 +0900

    add data-structure/fenwick-tree and verification

---
+++ data-structure/fenwick-tree/src/lib.rs
@@ -0,0 +1,54 @@
+use internal_trait::Zero;
+
+#[derive(Debug, Clone)]
+pub struct FenwickTree<T> {
+    tree: Vec<T>,
+    size: usize,
+}
+
+impl<T: Copy + Zero> FenwickTree<T>
+where
+    T: std::ops::Neg<Output = T>,
+    T: std::ops::Add<T, Output = T>,
+    T: std::ops::AddAssign,
+    T: std::ops::Sub<T, Output = T>,
+    T: std::ops::SubAssign,
+{
+    pub fn new(n: usize) -> Self {
+        let size = n;
+        Self {
+            tree: vec![T::zero(); size],
+            size,
+        }
+    }
+
+    pub fn build(&mut self, v: Vec<T>) {
+        assert!(v.len() <= self.size);
+        for i in 0..v.len() {
+            self.add(i, v[i]);
+        }
+    }
+
+    pub fn add(&mut self, mut k: usize, x: T) {
+        assert!(k < self.size);
+        k += 1;
+        while k <= self.size {
+            self.tree[k - 1] += x;
+            k += k & k.wrapping_neg();
+        }
+    }
+
+    pub fn sum(&mut self, l: usize, r: usize) -> T {
+        assert!(l <= r && r <= self.size);
+        self.prefix_sum(r) - self.prefix_sum(l)
+    }
+
+    fn prefix_sum(&mut self, mut r: usize) -> T {
+        let mut s = T::zero();
+        while r > 0 {
+            s += self.tree[r - 1];
+            r -= r & r.wrapping_neg();
+        }
+        s
+    }
+}
--- verification/aizu-online-judge/dsl_2_b/src/main.rs
+++ verification/aizu-online-judge/dsl_2_b/src/main.rs
@@ -2,33 +2,32 @@
 
 use proconio::input;
 
-use segment_tree::SegmentTree;
+// use segment_tree::SegmentTree;
+use fenwick_tree::FenwickTree;
 
 fn main() {
     input! {
         n: usize,
         q: usize,
     }
-    let mut st = SegmentTree::<i64>::new(n, |a, b| a + b, 0);
-    st.build(vec![0; n]);
+    // let mut st = SegmentTree::<i64>::new(n, |a, b| a + b, 0);
+    let mut ft = FenwickTree::<i64>::new(n);
     for _ in 0..q {
-        input! {
-            query: usize,
-        }
+        input! { query: usize, }
         match query {
             0 => {
                 input! {
                     x: usize,
                     y: i64,
                 }
-                st.apply(x - 1, y);
+                ft.add(x - 1, y);
             }
             1 => {
                 input! {
                     x: usize,
                     y: usize,
                 }
-                println!("{}", st.prod(x - 1, y));
+                println!("{}", ft.sum(x - 1, y));
             }
             _ => unreachable!(),
         }
