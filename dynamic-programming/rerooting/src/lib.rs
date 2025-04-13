2760c9a35f280574690e33f0dd43b1c7cc94338c
naoya675 <taguchi.naoya675@mail.kyutech.jp>
2025-04-14 00:12:24 +0900

    add wrapper func (rerooting)

--- dynamic-programming/rerooting/src/lib.rs
+++ dynamic-programming/rerooting/src/lib.rs
@@ -31,13 +31,13 @@ pub struct Rerooting<
 }
 
 impl<
-    Cost: Copy + Default,
-    Data: Copy,
-    Merge: Fn(Data, Data) -> Data,
-    E: Fn() -> Data,
-    Leaf: Fn() -> Data,
-    Apply: Fn(Data, usize, usize, Cost) -> Data,
-> Rerooting<Cost, Data, Merge, E, Leaf, Apply>
+        Cost: Copy + Default,
+        Data: Copy,
+        Merge: Fn(Data, Data) -> Data,
+        E: Fn() -> Data,
+        Leaf: Fn() -> Data,
+        Apply: Fn(Data, usize, usize, Cost) -> Data,
+    > Rerooting<Cost, Data, Merge, E, Leaf, Apply>
 {
     pub fn new(n: usize, merge: Merge, e: E, leaf: Leaf, apply: Apply) -> Self {
         Self {
@@ -111,3 +111,26 @@ impl<
         }
     }
 }
+
+pub struct RerootingDiameter;
+
+impl RerootingDiameter {
+    pub fn new(
+        n: usize,
+    ) -> Rerooting<
+        usize,
+        usize,
+        impl Fn(usize, usize) -> usize,
+        impl Fn() -> usize,
+        impl Fn() -> usize,
+        impl Fn(usize, usize, usize, usize) -> usize,
+    > {
+        let merge = |a: usize, b: usize| std::cmp::max(a, b);
+        let e = || 0;
+        let leaf = || 0;
+        let apply = |a: usize, _: usize, _: usize, w: usize| a + w;
+        Rerooting::new(n, merge, e, leaf, apply)
+    }
+}
+
+// reference: https://atcoder.jp/contests/abc222/editorial/2749
