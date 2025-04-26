290d85db1ac24aef05a4b8cc8d9013312d725655
naoya675 <taguchi.naoya675@mail.kyutech.jp>
2025-04-27 00:57:20 +0900

    implement suffix array (sa-is)

--- string/suffix-array/src/lib.rs
+++ string/suffix-array/src/lib.rs
@@ -63,7 +63,137 @@ impl SuffixArray {
     }
 
     fn sa_is(s: &Vec<usize>, upper: usize) -> Vec<usize> {
-        todo!()
+        let n = s.len();
+        if n == 0 {
+            return vec![];
+        }
+        if n == 1 {
+            return vec![0];
+        }
+        if n == 2 {
+            return if s[0] < s[1] { vec![0, 1] } else { vec![1, 0] };
+        }
+
+        let mut sa = vec![None; n];
+        let mut ls = vec![0; n];
+        for i in (0..n - 1).rev() {
+            if s[i] == s[i + 1] {
+                ls[i] = ls[i + 1];
+            } else if s[i] < s[i + 1] {
+                ls[i] = 1;
+            } else if s[i] > s[i + 1] {
+                ls[i] = 0;
+            }
+        }
+        let mut sum_l = vec![0; upper + 1];
+        let mut sum_s = vec![0; upper + 1];
+        for i in 0..n {
+            if ls[i] == 0 {
+                sum_s[s[i]] += 1;
+            } else {
+                sum_l[s[i] + 1] += 1;
+            }
+        }
+        for i in 0..upper + 1 {
+            sum_s[i] += sum_l[i];
+            if i < upper {
+                sum_l[i + 1] += sum_s[i];
+            }
+        }
+
+        let induce = |lms: &Vec<usize>, sa: &mut Vec<Option<usize>>| {
+            sa.fill(None);
+            let mut buf = sum_s.clone();
+            for &d in lms {
+                if d == n {
+                    continue;
+                }
+                sa[buf[s[d]]] = Some(d);
+                buf[s[d]] += 1;
+            }
+            let mut buf = sum_l.clone();
+            sa[buf[s[n - 1]]] = Some(n - 1);
+            buf[s[n - 1]] += 1;
+            for i in 0..n {
+                if let Some(v) = sa[i] {
+                    if v >= 1 && ls[v - 1] == 0 {
+                        sa[buf[s[v - 1]]] = Some(v - 1);
+                        buf[s[v - 1]] += 1;
+                    }
+                }
+            }
+            let mut buf = sum_l.clone();
+            for i in (0..n).rev() {
+                if let Some(v) = sa[i] {
+                    if v >= 1 && ls[v - 1] != 0 {
+                        buf[s[v - 1] + 1] -= 1;
+                        sa[buf[s[v - 1] + 1]] = Some(v - 1);
+                    }
+                }
+            }
+        };
+
+        let mut lms_map = vec![None; n + 1];
+        let mut m = 0;
+        for i in 1..n {
+            if ls[i - 1] == 0 && ls[i] != 0 {
+                lms_map[i] = Some(m);
+                m += 1;
+            }
+        }
+        let mut lms = vec![];
+        lms.reserve(m);
+        for i in 1..n {
+            if ls[i - 1] == 0 && ls[i] != 0 {
+                lms.push(i);
+            }
+        }
+        induce(&lms, &mut sa);
+        if m > 0 {
+            let mut sorted_lms = vec![];
+            sorted_lms.reserve(m);
+            for &v_option in &sa {
+                if let Some(v) = v_option {
+                    if lms_map[v].is_some() {
+                        sorted_lms.push(v);
+                    }
+                }
+            }
+            let mut rec_s = vec![0; m];
+            let mut rec_upper = 0;
+            rec_s[lms_map[sorted_lms[0]].unwrap()] = 0;
+            for i in 1..m {
+                let mut l = sorted_lms[i - 1];
+                let mut r = sorted_lms[i];
+                let end_l = if lms_map[l].unwrap() + 1 < m { lms[lms_map[l].unwrap() + 1] } else { n };
+                let end_r = if lms_map[r].unwrap() + 1 < m { lms[lms_map[r].unwrap() + 1] } else { n };
+                let mut same = true;
+                if end_l - l != end_r - r {
+                    same = false;
+                } else {
+                    while l < end_l {
+                        if s[l] != s[r] {
+                            break;
+                        }
+                        l += 1;
+                        r += 1;
+                    }
+                    if l == n || s[l] != s[r] {
+                        same = false;
+                    }
+                }
+                if !same {
+                    rec_upper += 1;
+                }
+                rec_s[lms_map[sorted_lms[i]].unwrap()] = rec_upper;
+            }
+            let rec_sa = SuffixArray::sa_is(&rec_s, rec_upper);
+            for i in 0..m {
+                sorted_lms[i] = lms[rec_sa[i]];
+            }
+            induce(&sorted_lms, &mut sa);
+        }
+        sa.into_iter().map(|a| a.unwrap()).collect()
     }
 
     pub fn suffix_array<T: Copy + Ord + PartialOrd>(s: &Vec<T>) -> Vec<usize> {
@@ -78,6 +208,6 @@ impl SuffixArray {
             }
             t[idx[i]] = now;
         }
-        Self::sa_doubling(&t)
+        Self::sa_is(&t, now)
     }
 }
