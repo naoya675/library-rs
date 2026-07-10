b21ef18b5744d6587dedeb3eea105297753b7014
naoya675 <taguchi.naoya675@mail.kyutech.jp>
2026-07-10 22:39:59 +0900

    implement crt

---
+++ docs/math/crt.md
@@ -0,0 +1,12 @@
+---
+title: Chinese Remainder Theorem
+documentation_of: //math/crt/src/lib.rs
+---
+
+## Reference
+- [https://atcoder.github.io/ac-library/production/document_en/math.html](https://atcoder.github.io/ac-library/production/document_en/math.html)
+- [https://info.atcoder.jp/entry/algorithm_lectures/chinese_remainder_theorem](https://info.atcoder.jp/entry/algorithm_lectures/chinese_remainder_theorem)
+- [https://qiita.com/drken/items/ae02240cd1f8edfc86fd](https://qiita.com/drken/items/ae02240cd1f8edfc86fd)
+
+## Verified
+- [https://codeforces.com/contest/338/problem/D](https://codeforces.com/contest/338/problem/D) ([submission](https://codeforces.com/contest/338/submission/382142347))
---
+++ math/crt/src/lib.rs
@@ -0,0 +1,35 @@
+use ext_gcd::ext_gcd;
+
+pub fn crt(r: &[i64], m: &[i64]) -> Option<(i64, i64)> {
+    assert_eq!(r.len(), m.len());
+    let mut r0 = 0;
+    let mut m0 = 1;
+    for i in 0..r.len() {
+        assert!(m[i] >= 1);
+        let mut r1 = r[i].rem_euclid(m[i]);
+        let mut m1 = m[i];
+        if m0 < m1 {
+            std::mem::swap(&mut r0, &mut r1);
+            std::mem::swap(&mut m0, &mut m1);
+        }
+        if m0 % m1 == 0 {
+            if r0 % m1 != r1 {
+                return None;
+            }
+            continue;
+        }
+        let (g, x, _) = ext_gcd(m0, m1);
+        if (r1 - r0) % g != 0 {
+            return None;
+        }
+        let u1 = m1 / g;
+        let im = x.rem_euclid(u1);
+        let k = (r1 - r0) / g % u1 * im % u1;
+        r0 += k * m0;
+        m0 *= u1;
+        if r0 < 0 {
+            r0 += m0;
+        }
+    }
+    Some((r0, m0))
+}
