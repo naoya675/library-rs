f9af0e38c4384b2927ef8c8740a5478d3169e0ae
naoya675 <taguchi.naoya675@mail.kyutech.jp>
2026-07-14 13:50:05 +0900

    verify number_of_subsequences

---
+++ verification/library-checker/enumerative_combinatorics/number_of_subsequences/src/main.rs
@@ -0,0 +1,33 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/number_of_subsequences
+
+// Reference:
+// - https://noshi91.hatenablog.com/entry/2023/02/26/135340
+// - https://atcoder.jp/contests/abc446/editorial/16429
+
+use proconio::input;
+
+use lower_bound::LowerBound;
+use modint::Modint;
+
+type Mint = Modint<998244353>;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+    let mut sorted = a.clone();
+    sorted.sort();
+    sorted.dedup();
+    let a: Vec<usize> = a.iter().map(|v| sorted.lower_bound(v)).collect();
+
+    let mut dp = vec![Mint::new(0); a.len() + 1];
+    let mut sum = Mint::new(1);
+    for i in 0..n {
+        let tmp = dp[a[i]];
+        dp[a[i]] = sum;
+        sum += sum - tmp;
+    }
+
+    println!("{}", sum - Mint::new(1));
+}
