c4f317788be7bead12b3c42a8cc8a57ac124a46e
naoya675 <taguchi.naoya675@mail.kyutech.jp>
2026-07-31 17:52:22 +0900

    verify point_set_range_frequency

--- docs/data-structure/persistent-array.md
+++ docs/data-structure/persistent-array.md
@@ -6,4 +6,5 @@ documentation_of: //data-structure/persistent-array/src/lib.rs
 ## Reference
 - P. F. Dietz, ["Fully persistent arrays"](https://link.springer.com/chapter/10.1007/3-540-51542-9_8), Workshop on Algorithms and Data Structures (1989), 67–74.
 - [https://37zigen.com/persistent-array/](https://37zigen.com/persistent-array/)
+- [https://qiita.com/hotman78/items/9c643feae1de087e6fc5](https://qiita.com/hotman78/items/9c643feae1de087e6fc5)
 - [https://trap.jp/post/663/](https://trap.jp/post/663/)
---
+++ verification/library-checker/data_structure/point_set_range_frequency/src/main.rs
@@ -0,0 +1,49 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/point_set_range_frequency
+
+use std::collections::HashMap;
+
+use proconio::input;
+
+use treap::Treap;
+
+query::define_query! {
+    Query {
+        0 => Query0(k: usize, v: i64),
+        1 => Query1(l: usize, r: usize, x: i64),
+    }
+}
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        mut a: [i64; n],
+        queries: [Query; q],
+    }
+    let mut pos: HashMap<i64, Treap<usize>> = HashMap::new();
+    for (i, &x) in a.iter().enumerate() {
+        pos.entry(x).or_insert(Treap::new()).insert(i);
+    }
+    let mut res = vec![];
+
+    for &query in &queries {
+        match query {
+            Query0(k, v) => {
+                pos.get_mut(&a[k]).unwrap().remove(&k);
+                pos.entry(v).or_insert(Treap::new()).insert(k);
+                a[k] = v;
+            }
+            Query1(l, r, x) => {
+                let cnt = match pos.get(&x) {
+                    Some(t) => t.lower_bound(&r) - t.lower_bound(&l),
+                    None => 0,
+                };
+                res.push(cnt);
+            }
+        }
+    }
+
+    for &v in &res {
+        println!("{}", v);
+    }
+}
